c338cdd75a85c9d34d18d3b2e03baf3e059e73bc
Hai <truonghai29012901@gmail.com>
2019-09-29 13:35:52 -0400

    Added Sprite and Scripts
    For background and player controller

---
+++ Assets/_Scripts/GalaxyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyController : MonoBehaviour
+{
+    public float verticalSpeed = 0.1f;
+    public float resetPoint = -6.2f;
+    public float resetPosition = 6.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Check();
+    }
+    /// <summary>
+    /// This method moves the galaxy by verticalSpeed
+    /// </summary>
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(0.0f, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+    /// <summary>
+    /// This method reset the galaxy to the resetPosition
+    /// </summary>
+    void Reset()
+    {
+        transform.position = new Vector2(0.0f, resetPosition);
+    }
+    /// <summary>
+    /// This method check the condition(resetPoint) if it reach the resetpoint
+    /// it Reset
+    /// </summary>
+    void Check()
+    {
+        if (transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+public class PlayerController : MonoBehaviour
+{
+    public Speed speed;
+    public Boundary boundary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        CheckBound();
+    }
+    /// <summary>
+    /// This method use to move the player in x axis only
+    /// </summary>
+    void move ()
+    {
+        Vector2 newPosition = transform.position;
+        if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            newPosition += new Vector2(speed.max, 0.0f);
+        }
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            newPosition += new Vector2(speed.min, 0.0f);
+        }
+        transform.position = newPosition;
+    }
+    /// <summary>
+    /// Restrict player from move outside the scene
+    /// by using boundary class
+    /// </summary>
+    public void CheckBound()
+    {
+        //Check right boundary
+        if (transform.position.x > boundary.Right)
+        {
+            transform.position = new Vector2(boundary.Right, transform.position.y);
+        }
+        //Check left boundary
+        if (transform.position.x < boundary.Left)
+        {
+            transform.position = new Vector2(boundary.Left, transform.position.y);
+        }
+    }
+}

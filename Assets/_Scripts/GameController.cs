01dd2e383bdb224ca4b7c92e3d48bee0b75c6145
Hai <truonghai29012901@gmail.com>
2019-10-01 10:08:23 -0400

    Added meteor scripts and meteor animation

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Scene game object")]
+    public GameObject meteor;
+
+    public int numberOfMeteor;
+    public List<GameObject> meteors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create empty container (list) of type GameObject
+        meteors = new List<GameObject>();
+        for (int i = 0; i < numberOfMeteor; i++)
+        {
+            meteors.Add(Instantiate(meteor));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/MeteorScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class MeteorScript : MonoBehaviour
+{
+    [Header("Speed Values")]
+    [SerializeField]
+    public Speed horizontalSpeedRange;
+
+    [SerializeField]
+    public Speed verticalSpeedRange;
+
+    public float verticalSpeed;
+    public float horizontalSpeed;
+
+    [SerializeField]
+    public Boundary boundary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    /// <summary>
+    /// This method moves the Meteor down the screen by verticalSpeed
+    /// </summary>
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(horizontalSpeed, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    /// <summary>
+    /// This method resets the Meteor to the resetPosition
+    /// </summary>
+    void Reset()
+    {
+        horizontalSpeed = Random.Range(horizontalSpeedRange.min, horizontalSpeedRange.max);
+        verticalSpeed = Random.Range(verticalSpeedRange.min, verticalSpeedRange.max);
+        float randomXPosition = Random.Range(boundary.Left, boundary.Right);
+        transform.position = new Vector2(randomXPosition, Random.Range(boundary.Top, boundary.Top + 2.0f));
+    }
+
+    /// <summary>
+    /// This method checks if the Meteor reaches the lower boundary
+    /// and then it Resets it
+    /// </summary>
+    void CheckBounds()
+    {
+        if (transform.position.y <= boundary.Bottom)
+        {
+            Reset();
+        }
+    }
+}
+
